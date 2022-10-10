28f35d0dd4799edf52ea3b6605b80dbef73be444
sensational.mack <mackn478@gmail.com>
2022-10-10 16:45:15 -0400

    recent changes to index-js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 var today = moment();
-$("#currentDay").text(moment().format("MMMM ,  Do YYYY"));
-var time = moment().format("h:mm:ss a")
+$("#currentDay").text(moment().format("MMMM ,  Do YYYY, h:mm:ss a"));
 console.log(time);
