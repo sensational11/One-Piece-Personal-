2c4b52e74cc7a22676e6093df6cbb6ae514d03ff
sensational.mack <mackn478@gmail.com>
2022-10-13 00:24:21 -0400

    recent changes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var searchInput = "";
 var characterEL = $("#Character");
 var devilFruitPowers = $("#Devil-Fruit-Powers");
 var episodeInformation = $("#Episode-Information");
-var apiKey = "b6ae296a9dmsh55f69a0c5e3f383p1d0a0fjsned942c7c01cb";
+// var apiKey = "b6ae296a9dmsh55f69a0c5e3f383p1d0a0fjsned942c7c01cb";
 
 searchButton.click(function () {
   searchInput = $("#form-control").val;
@@ -14,6 +14,15 @@ searchButton.click(function () {
 });
 
 function getSelection(searchInput) {
-  var apiUrl = "rapidapi.com" + searchInput + "application_6750808" + apiKey;
-  
+  // var apiUrl = "rapidapi.com" + searchInput + "application_6750808" + apiKey;
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+        var classList = data[0];
+        className = data[0].name;
+        getSelection(classList);
+      });
+    }
+  });
 }
